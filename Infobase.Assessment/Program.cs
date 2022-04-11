d768f70108972b2f098814510132c1a56afae10a
User <User@DESKTOP-K1HGS43>
2022-04-11 11:09:12 +0530

    Add project files.

---
+++ Infobase.Assessment/Program.cs
@@ -0,0 +1,61 @@
+﻿using Infobase.Assessment;
+using System.Diagnostics;
+using System.Reflection;
+
+Stopwatch main_timer = new();
+Stopwatch file_timer = new();
+Console.WriteLine("Timer Started...\n\n");
+main_timer.Start();
+
+//Initial data
+var input_file_name = @"App_Data\words.txt";
+var output_uniques_file_name = @"App_Data\uniques.txt";
+var output_fullwords_file_name = @"App_Data\fullwords.txt";
+//var folder_path = @"D:\VS_STUDIO_PROJECTS\Infobase.Assessment\Infobase.Assessment\Files";
+//var folder_path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+var folder_path = Path.GetDirectoryName(Environment.ProcessPath);
+
+var utilities = new Utilities();
+
+Console.WriteLine("trying to read the content from input file : words.text ...");
+var input_file_path = Path.Combine(folder_path, input_file_name);
+if (!File.Exists(input_file_path))
+    Console.Error.WriteLine("File does not exist!");
+
+// Open the file to read from.
+file_timer.Start();
+string[] words = File.ReadAllLines(input_file_path);
+file_timer.Stop();
+Console.WriteLine("Elapsed time to read file : " + file_timer.ElapsedMilliseconds + "\n\n");
+int char_num = 4;
+
+Console.WriteLine("Processing the file words to find unique " + char_num + " letter sequential words...");
+var seqCombinations = utilities.GetUniqueNLetterSequences(words.ToList<string>(), char_num);
+
+
+Console.WriteLine("Total Unique combinations : " + seqCombinations.Count + "\n\n");
+
+var sortedSeqCombinations = new SortedDictionary<string, string>(seqCombinations);
+
+//Write content to files..
+var output_uniques_filepath = Path.Combine(folder_path, output_uniques_file_name);
+var output_fullwords_filepath = Path.Combine(folder_path, output_fullwords_file_name);
+Console.WriteLine("Trying to write content to out files....");
+file_timer.Start();
+
+//Parallel.Invoke(
+//    () => utilities.WriteMultiLinesToFile(sortedSeqCombinations.Keys.ToList<string>(), output_uniques_filepath),
+//    () => utilities.WriteMultiLinesToFile(sortedSeqCombinations.Values.ToList<string>(), output_fullwords_filepath)
+//    );
+
+utilities.WriteMultiLinesToFile(sortedSeqCombinations.Keys.ToList<string>(), output_uniques_filepath);
+utilities.WriteMultiLinesToFile(sortedSeqCombinations.Values.ToList<string>(), output_fullwords_filepath);
+
+file_timer.Stop();
+Console.WriteLine("OUTPUT (Uniques) : " + output_uniques_filepath);
+Console.WriteLine("OUTPUT (fullwords) : " + output_fullwords_filepath);
+Console.WriteLine("Elapsed time to write to output files : " + file_timer.ElapsedMilliseconds + " milliseconds. \n\n");
+
+main_timer.Stop();
+Console.WriteLine("Total Elapsed time : " + main_timer.ElapsedMilliseconds + " milliseconds");
+Console.ReadLine();
---
+++ Infobase.Assessment/Utilities.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infobase.Assessment
+{
+    internal class Utilities
+    {
+        /// <summary>
+        /// returns all possible combination of sequential groups with N number of characters/letters in a given word
+        /// </summary>
+        /// <param name="word">input word</param>
+        /// <param name="char_num">number of characters in each sequential group</param>
+        /// <returns>A list of all possible combinations of N letter sequences</returns>
+        private List<string>? GetNLetterSequentialWords(string word, int char_num)
+        {
+            if (word.Length < char_num) return null;
+            var seqWords = new List<string>();
+            for (int i = 0; i <= word.Length - char_num; i++)
+                seqWords.Add((word.Substring(i, char_num)).ToLower());
+            return seqWords;
+        }
+
+        /// <summary>
+        /// Write content (multiple lines) to a file corresponsing to given path
+        /// </summary>
+        /// <param name="lines">a string array, each item representing a line in the output file</param>
+        /// <param name="filePath">complete file path : the location, file name and the extension</param>
+        /// <returns>void</returns>
+        /// <exception cref="Exception"></exception>
+        public void WriteMultiLinesToFile(List<string> lines, string filePath)
+        {
+            try
+            {
+                File.WriteAllLines(filePath, lines);
+            }
+            catch (Exception err)
+            {
+                throw new Exception("An Error occured while writing to file. Details : \n" + err.Message);
+            }
+        }
+
+        /// <summary>
+        /// Takes in a group of words and returns all possible unique combinations of N letter sequential words
+        /// </summary>
+        /// <param name="words"></param>
+        /// <param name="char_num"></param>
+        /// <returns></returns>
+        public IDictionary<string, string> GetUniqueNLetterSequences(List<string> words, int char_num)
+        {
+            IDictionary<string, List<string>> seqCombinations = new Dictionary<string, List<string>>();
+            foreach (string word in words)
+            {
+                if (word.Length >= char_num)
+                {
+                    var seqWords = GetNLetterSequentialWords(word, char_num);
+                    if (seqWords != null && seqWords.Count > 0)
+                    {
+                        foreach (string seq in seqWords)
+                        {
+                            if (seqCombinations.ContainsKey(seq))
+                                seqCombinations[seq].Add(word);
+                            else
+                                seqCombinations.Add(seq, new List<string> { word });
+                        }
+                    }
+                }
+            }
+
+            var uniqueSeqCombinations = seqCombinations.AsParallel()
+                                        .Where(seqComb => seqComb.Value.Count == 1)
+                                        .ToDictionary(seqComb => seqComb.Key, seqComb => seqComb.Value.First());
+
+            return uniqueSeqCombinations;
+        }
+    }
+}
